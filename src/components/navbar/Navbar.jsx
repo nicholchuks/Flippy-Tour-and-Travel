acffa468ab4ffa710a683621518a132dee025853
Nicholas chuks <niccobery91@gmail.com>
2024-07-22 16:52:13 +0100

    created the navbar and also made it responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,22 +3,34 @@ import { MenuItems } from "../MenuItem";
 import "./NavbarStyle.css";
 
 class Navbar extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
   render() {
     return (
       <nav className="NavbarItems">
         <h1 className="navbar-logo">Trippy</h1>
-        <ul className="nav-menu">
-          <li>
-            {MenuItems.map((item, index) => {
-              return (
-                <li key={index}>
-                  <a href={item.url}>
-                    <i className={item.icon}></i>{item.title}
-                  </a>
-                </li>
-              );
-            })}
-          </li>
+
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+          {/* <i className="fas fa-times"></i> */}
+        </div>
+
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  <i className={item.icon}></i>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+          <button>Sign Up</button>
         </ul>
       </nav>
     );
