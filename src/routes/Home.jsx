81d687f1cfdbf78aa011464c1f63caca0ebcd2c6
Nicholas chuks <niccobery91@gmail.com>
2024-07-28 00:41:26 +0100

    made little adjustment

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -12,7 +12,6 @@ const Home = () => {
       <Hero
         cName="hero"
         heroImg={HeroMainImg}
-        // heroImg="https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?q=80&w=1574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         title="Your Journey Your Story"
         text="Choose Your Favorite Destination."
         buttonText="Travel Plan"
