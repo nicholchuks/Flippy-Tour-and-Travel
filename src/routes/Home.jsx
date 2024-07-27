d0d37e6149df3d6e60e9d78f9d996b5be7ffeab0
Nicholas chuks <niccobery91@gmail.com>
2024-07-28 00:39:41 +0100

    Changed the hero Img

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,6 +3,7 @@ import Footer from "../components/footer/Footer";
 import Hero from "../components/hero/Hero";
 import Navbar from "../components/navbar/Navbar";
 import Trip from "../components/trip/Trip";
+import HeroMainImg from "../assets/12.jpg";
 
 const Home = () => {
   return (
@@ -10,7 +11,8 @@ const Home = () => {
       <Navbar />
       <Hero
         cName="hero"
-        heroImg="https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?q=80&w=1574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        heroImg={HeroMainImg}
+        // heroImg="https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?q=80&w=1574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         title="Your Journey Your Story"
         text="Choose Your Favorite Destination."
         buttonText="Travel Plan"
