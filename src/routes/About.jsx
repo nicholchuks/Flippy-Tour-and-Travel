93c0312bd6b7f4ac22e3ce3a0f6eb7234abe0eba
Nicholas chuks <niccobery91@gmail.com>
2024-07-27 15:22:22 +0100

    all assets added successfully and created the about page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Navbar from "../components/navbar/Navbar";
+import AboutImg from "../assets/night.jpg";
+import Hero from "../components/hero/Hero";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <Navbar />
+      <Hero
+        cName="hero-mid"
+        heroImg={AboutImg}
+        title="Your Journey Your Story"
+        text="Choose Your Favorite Destination."
+        buttonText="Travel Plan"
+        url="/"
+        btnClass="show"
+      />
+    </>
+  );
 };
 
 export default About;
